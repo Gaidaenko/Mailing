9e462340ed2ce4368cb09f512bf0d012183b5020
Gaidaenko <45212496+Gaidaenko@users.noreply.github.com>
2022-01-13 13:31:59 +0200

    Отправка

--- Mailing/Program.cs
+++ Mailing/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,26 @@ namespace Mailing
     {
         static void Main(string[] args)
         {
+
+            SendMail();
+        }
+
+
+        static void SendMail()
+        {
+
+            MailAddress from = new MailAddress("sendtestmessages@gmail.com", "Тест рассылки");
+            MailAddress to = new MailAddress("yura@bitservice.ua");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Тема тестовой рассылки: ";
+            m.Body = ("Тестовая рассылка: ");
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("sendtestmessages@gmail.com", "Password");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+
+
         }
     }
 }
